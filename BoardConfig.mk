190859ec6193f91feafea0d3674539f38b4c5f1f
chuspita <kosecki99@gmail.com>
2023-06-08 17:18:34 +0200

    redwood: Set RELAX_USES_LIBRARY_CHECK := true

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,6 +7,7 @@
 DEVICE_PATH := device/xiaomi/redwood
 
 BUILD_BROKEN_DUP_RULES := true
+RELAX_USES_LIBRARY_CHECK := true
 
 # Inherit from sm7325-common
 include device/xiaomi/sm7325-common/BoardConfigCommon.mk
